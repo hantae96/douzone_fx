87367bb4ee61071270b3c747d47cab8edde6e64a
hantae <85547680+hantae96@users.noreply.github.com>
2023-03-28 23:36:38 +0900

    Feat ; 메모리 리포지토리 추가

--- market/src/main/java/com/fx/market/controller/PurchaseController.java
+++ market/src/main/java/com/fx/market/controller/PurchaseController.java
@@ -1,5 +1,11 @@
 package com.fx.market.controller;
 
+import java.util.Iterator;
+import java.util.List;
+
+import com.fx.market.Dto.ItemDto;
+import com.fx.market.repository.MemoryRepository;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -16,13 +22,20 @@ public class PurchaseController {
 	@FXML private Button submit;
 	
 	
+	MemoryRepository repository = new MemoryRepository();
+	
 	 @FXML
 	    protected void onSubmitButtonClick() {
 		 	Alert alert = new Alert(AlertType.WARNING);
 		 	alert.setTitle("거래 완료 창");
 		 	alert.setHeaderText("거래가 완료되었습니다.");
 		 	alert.setContentText("거래가 완료되었다고!!");
-		 	
 		 	alert.showAndWait();
+		 	
+		 	List<ItemDto> items = repository.findAll();
+		 	for (ItemDto itemDto : items) {
+				System.out.println(itemDto.getItemName());
+			}
 	    }
 }
+
