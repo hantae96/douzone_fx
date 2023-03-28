39f4d383f04ef905fe21b5a3be97068eae811f3d
hantae <85547680+hantae96@users.noreply.github.com>
2023-03-28 16:17:07 +0900

    git 테스트

--- market/src/main/java/com/fx/market/HelloApplication.java
+++ market/src/main/java/com/fx/market/HelloApplication.java
@@ -10,12 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
 
-        FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("views/main.fxml"));
         Scene main = new Scene(fxmlLoader1.load(), 320, 240);
         stage.setTitle("main");
         stage.setScene(main);
