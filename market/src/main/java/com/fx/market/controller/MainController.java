cf0d11a1589d696536ac1a048205d96d03e87108
hantae <85547680+hantae96@users.noreply.github.com>
2023-03-28 16:23:32 +0900

    test git

---
+++ market/src/main/java/com/fx/market/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.fx.market.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Button hantae;
+    @FXML private Label testTitle;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    
+    @FXML
+    protected void hantaeClick() {
+    	testTitle.setText("method 테스트");
+    	System.out.println("test!!");
+    }
+}
