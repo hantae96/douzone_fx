592a06e85099b8c25ffd1cf1df92ec3ecdbce357
ragoha92 <ragoha92@naver.com>
2023-03-30 08:34:52 +0900

    Feat : 동네생활 함께해요 게시글 작성 기능 추가 #2

--- market/src/main/java/com/fx/market/HelloApplication.java
+++ market/src/main/java/com/fx/market/HelloApplication.java
@@ -29,12 +29,12 @@ public class HelloApplication extends Application {
 //        stage.show();
 //        
 // 홈 화면 테스트
-		FXMLLoader homeFxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/home.fxml"));
-		Scene home = new Scene(homeFxmlLoader.load(), 370, 650);
-		stage.setTitle("홈 화면");
-		stage.setScene(home);
+//		FXMLLoader homeFxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/home.fxml"));
+//		Scene home = new Scene(homeFxmlLoader.load(), 370, 650);
+//		stage.setTitle("홈 화면");
+//		stage.setScene(home);
 
-<<<<<<< HEAD
+
        	
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/main.fxml"));
         System.out.println(HelloApplication.class.getResource("views/main.fxml"));
@@ -56,9 +56,9 @@ public class HelloApplication extends Application {
         MainController mainController = fxmlLoader.getController();
         mainController.setViewer(viewer);
         
-=======
-    	HomeController homeController = new HomeController();
->>>>>>> main
+
+//    	HomeController homeController = new HomeController();
+
         stage.show();
     	
     }
