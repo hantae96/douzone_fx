0760d545aa09b7232d509fa4be1478d226fa0a9f
hantae <85547680+hantae96@users.noreply.github.com>
2023-03-30 01:55:58 +0900

    Feat : 리스트 출력 기능 추가 및 UI 수정

--- market/src/main/java/com/fx/market/controller/HomeController.java
+++ market/src/main/java/com/fx/market/controller/HomeController.java
@@ -3,15 +3,25 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import com.fx.market.dao.HomeDao;
 import com.fx.market.dto.HomeDto;
+import com.fx.market.service.HomeService;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 
 // initalize 는 fxml 로더가 fx 변수를 모두 파싱한다음에 호출됨
@@ -21,19 +31,60 @@ import javafx.scene.layout.VBox;
 public class HomeController implements Initializable{
 	
 	@FXML VBox main;
+	HomeService homeService;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		homeService = new HomeService();
 		printAllItem();
 	}
 
 	public void printAllItem() {
-		HomeDao dao = new HomeDao();
-		List<HomeDto> items = dao.getItemAll();
+		List<HomeDto> items =homeService.makeViewItem();
+		
 		for (HomeDto item : items) {
-				Label label = new Label(item.getItemName());
-				main.getChildren().add(label);
-		}
+			
+			Label name = new Label(item.getItemName());
+			name.setPadding(new Insets(10));
+			// 폰트 크기 변경
+			name.setFont(new Font(16));
+			// 굵기 변경
+			name.setFont(Font.font("System", FontWeight.BOLD, 16));
+			Label address = new Label(item.getAddress());
+//			address.setPadding(new Insets(10));
+			Label price = new Label(item.getPrice());
+			price.setPadding(new Insets(0,0,5,20));
+
+			Label recommand;
+			
+			if (item.getRecommend() != 0) {
+				recommand = new Label("♡".concat(String.valueOf(item.getRecommend())));
+			}else{
+				recommand = new Label("");
+			}
+		
+			BorderPane section = new BorderPane();
+			section.setBorder(new Border(new BorderStroke(Color.BLACK, 
+					BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+			
+			section.setTop(name);
+			section.setLeft(new Label("photo"));
+			section.getLeft().setStyle("-fx-border-color: black;");
+
+			section.setCenter(price);
+			BorderPane.setAlignment(price, Pos.CENTER_LEFT);
+
+			section.setBottom(address);
+			section.setRight(recommand);
+
+			section.setPadding(new Insets(10)); // 모든 방향에 대해 10px의 패딩 적용
+
+
+			main.getChildren().add(section);
+	
+
+	}
+
 	}
 
 	@FXML
--- market/src/main/java/module-info.java
+++ market/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.fx.market {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+	requires javafx.graphics;
 
     opens com.fx.market to javafx.fxml;
     exports com.fx.market;
