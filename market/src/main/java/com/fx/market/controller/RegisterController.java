5abe2d30b6a253f8cf47ee4cbbd94f041b46237f
hantae <85547680+hantae96@users.noreply.github.com>
2023-03-31 01:35:09 +0900

    Feat : 상세 페이지 취소 버튼 추가

--- market/src/main/java/com/fx/market/controller/RegisterController.java
+++ market/src/main/java/com/fx/market/controller/RegisterController.java
@@ -1,15 +1,27 @@
 package com.fx.market.controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import com.fx.market.common.Viewer;
 import com.fx.market.dao.RegisterDao;
 import com.fx.market.dto.ItemDto;
 import com.fx.market.service.RegisterService;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegisterController implements Initializable {
 	RegisterService registerService;
@@ -25,23 +37,40 @@ public class RegisterController implements Initializable {
 
 	@FXML
 	private TextField address;
+	@FXML Button close;
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.registerService = new RegisterService(new RegisterDao());
-		price.textProperty().addListener((observable, oldValue, newValue) -> {
-	        // 새로 입력된 문자열이 숫자가 아니면, 이전 값으로 되돌립니다.
-	        if (!newValue.matches("\\d*")) {
-	            price.setText(oldValue);
-	        }
-	    });
-	}
+		checkPrice(price);
 
+	}
+	
+	
 	@FXML
 	private void onRegsistButtonClick() {
 		registerService.saveItemData(
 				new ItemDto(name.getText(), Long.valueOf(price.getText()), context.getText(), address.getText()));
 	}
 	
-	
+	@FXML
+	private void onCancelButtonClick() {	
+		Viewer viewer = new Viewer();
+		viewer.setView("home");
+	}
+
+	private void checkPrice(TextField price) {
+		// 가격 입력값 유효성검사
+		price.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue.matches("\\d*")) {
+				price.setText(oldValue);
+				price.setStyle("-fx-text-fill: red;");
+				price.setPromptText("가격");
+			} else {
+				price.setStyle("-fx-text-fill: black;");
+				price.setPromptText("");
+			}
+		});
+	}
 }
