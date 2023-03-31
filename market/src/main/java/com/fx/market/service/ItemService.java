679ae3d66a6fc945c10960550d394a672e0600e8
hantae <85547680+hantae96@users.noreply.github.com>
2023-03-31 22:47:53 +0900

    Feat : 상세 페이지 상세 정보 출력 기능 추가

--- market/src/main/java/com/fx/market/service/ItemService.java
+++ market/src/main/java/com/fx/market/service/ItemService.java
@@ -2,11 +2,18 @@ package com.fx.market.service;
 
 import com.fx.market.dao.ItemDao;
 import com.fx.market.dto.ItemDto;
+import com.fx.market.dto.ItemUserDto;
 
 public class ItemService {
 
 	public ItemDto getItemById(String itemId) {
 		ItemDao dao = new ItemDao();
 		return dao.getItemById(itemId);
+	}
+	
+	public ItemUserDto getUserById(String itemId) {
+		ItemDao dao = new ItemDao();
+		ItemUserDto dto =dao.getUserById(itemId);
+		return dto;
 	}
 }
